8dad63dbb5509790f58ae7105b9e215446eef7cc
transgenderethiopian <janimelody@gmail.com>
2017-06-05 19:37:11 +0200

    addedshit

---
+++ Jani.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <li>bricks</li>
+    <li>mortar</li>
+  </body>
---
+++ ethiopian.js
@@ -0,0 +1,6 @@
+var x = 'jani +'
+var f = ' lana'
+var love = x + f
+console.log(love);
+
+console.log('4 realz');
